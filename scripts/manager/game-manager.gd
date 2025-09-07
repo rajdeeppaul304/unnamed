8dda6c71f7fcf25d6185f53a347783bb31b240d8
simplycode07 <simplycode07@gmail.com>
2025-09-07 20:01:25 +0530

    changed json format for the decision tree

--- scripts/manager/game-manager.gd
+++ scripts/manager/game-manager.gd
@@ -9,6 +9,13 @@ var decision_tree: DecisionTree
 func _ready():
 	decision_tree = DecisionTree.new()
 	decision_tree.load_from_json("res://data/level1.json")
-	var root := decision_tree.find_node_by_id("root")
+	var root := decision_tree.root
+	
+	decision_tree.find_node_by_id("0")
+	decision_tree.find_node_by_id("0_1")
+	decision_tree.find_node_by_id("0_2")
+	decision_tree.find_node_by_id("0_3")
+	
+	
 	
 	#decision_tree.init("asdf", scenes_dict["level_1_scene"], )
